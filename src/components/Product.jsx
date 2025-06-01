2c2c7713784c757b628102f4d2e57d7feaba80e3
gajamvarsha227 <23r11a0518@gcet.edu.in>
2025-06-01 19:40:02 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-
+import React from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../App'
 export default function Product() {
-  return <div>Product List</div>;
+  const {user} = useContext(AppContext)
+  return (
+    <div>
+      <h3>Welcome {user.name}!</h3>
+      Product List
+      </div>
+  )
 }
